b3f9c9d357274e24c6cf5c59d1c0ba29035d6a6c
Alan <alan.jordy1999@gmail.com>
2023-02-06 11:51:07 +0100

    navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="navbar">
+        <ul>
+            <li><img src="../assets/baleine.png" alt="image baleine bleu" style="height: 50px;" /></li>
+            <li><span>Baleine bleu </span></li>
+            <li><a href="">Produit </a></li>
+            <li><a href="">Sign in </a></li>
+            <li><a href=""> Log in</a></li>
+        </ul>
+        
+        
+        
+        
+        
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+li {
+  float: left;
+  display: inline;
+}
+
+</style>
