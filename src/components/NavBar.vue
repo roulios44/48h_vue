61f5cb0769cd76cc1901341b3efd85b6269a4f9e
Alan <alan.jordy1999@gmail.com>
2023-02-06 12:18:09 +0100

    style navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
     <div class="navbar">
         <ul>
             <li><img src="../assets/baleine.png" alt="image baleine bleu" style="height: 50px;" /></li>
-            <li><span>Baleine bleu </span></li>
+            <li><a href="">Baleine bleu </a></li>
             <li><a href="">Produit </a></li>
             <li><a href="">Sign in </a></li>
             <li><a href=""> Log in</a></li>
@@ -25,9 +25,27 @@ export default {
 </script>
 
 <style>
-li {
+.navbar ul {
+    position: fixed;
+    list-style-type: none;
+    margin: 0;
+    top: 0;
+    right: 0;
+    left: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+  }
+.navbar li {
   float: left;
-  display: inline;
+}
+
+.navbar li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
 }
 
 </style>
