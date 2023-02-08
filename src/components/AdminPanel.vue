c2b0c684d2cc2a2861145b98286a68180ee8449b
roulios44 <jules.croizier@gmail.com>
2023-02-08 02:09:17 +0100

    manualy add server => OK

--- src/components/AdminPanel.vue
+++ src/components/AdminPanel.vue
@@ -1,7 +1,16 @@
 <template>
     <h1>Panel Administrateur</h1>
-    <div class="box">
-      
+    <div class="listOfIp">
+      <label for="server">Serveur available :</label>
+      <span v-for="site in available">{{ site.serverIp }}</span>
+      <br />
+      <h1>Manually add server</h1>
+      <label>Ip Address</label>
+      <input class="panier" type="text" placeholder="Ip Address" v-model="ipToAdd"/>
+      <input type="button" v-on:click="addServer()"/>
+
+    </div>
+    <div class="blackList">
       <label for="server">Serveur black-list :</label>
       <span v-for="site in blackList">{{ site.name }}    {{ site.addressIp }}</span>
       <br />
@@ -24,6 +33,9 @@ export default {
       blackList: [],
       ipToBlackList:"",
       nameToBlackList:"",
+
+      available:[],
+      ipToAdd:"",
     }
   },
   methods:{
@@ -38,26 +50,51 @@ export default {
         "name":this.nameToBlackList
       }))
       const res = await post.data
-      console.log(res)
       this.getAllBlackList()
+    },
+    async getAllAvailableIp(){
+      const req = await axios.get("http://localhost/48h_back/getAllIp.php")
+      const data = await req.data
+      this.available = data
+    },
+    async addServer(){
+      console.log(this.ipToAdd)
+      const post = await axios.post("http://localhost/48h_back/addIpAvailable.php",JSON.stringify({
+        "ip":this.ipToAdd,
+      }))
+      const res = await post.data
+      console.log(res)
+      this.getAllAvailableIp()
     }
   },
   async mounted(){
     await this.getAllBlackList()
+    await this.getAllAvailableIp()
   }
 }
 </script>
 
 <style>
 
-.box {
+.blackList {
     display: inline-block;
     margin-left: auto;
     margin-right: auto;
     border: 3px solid !important;
     border-radius: 20px !important;
     border-color: white !important;
-    width: 30%;
+    width: 50%;
+    margin: 100px;
+}
+.listOfIp{
+  display: inline-block;
+  margin-left: auto;
+  margin-right: auto;
+  border: 3px solid !important;
+  border-radius: 20px !important;
+  border-color: white !important;
+  width: 50%;
+  margin: 100px;
 }
 
 .card .panier{
